d2d65e25af2effec0111d8401f2c7bacd85cdc4d
vo0a <vo0a195@gmail.com>
2021-01-19 22:56:42 +0900

    210119 boj 문제 추가

---
+++ 210119_boj_2252_줄 세우기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int n, m, indegree[32001];
+vector<vector<int>> adj;
+
+int main() {
+	scanf("%d %d", &n, &m);
+	adj.resize(n + 1);
+	for (int i = 0, a, b; i < m; i++) {
+		scanf("%d%d", &a, &b);
+		adj[a].push_back(b);
+		indegree[b]++;
+	}
+	priority_queue<int> pq;
+	for (int i = 1; i <= n; i++) {
+		if (!indegree[i]) {
+			pq.push(i);
+		}
+	}
+	while (!pq.empty()) {
+		int front = pq.top();
+		pq.pop();
+		printf("%d ", front);
+		for (auto e : adj[front]) {
+			if (--indegree[e] == 0) {
+				pq.push(e);
+			}
+		}
+	}
+
+	return 0;
+}
